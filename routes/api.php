65bde11dd48347ab08ea39fd621b20dd289f972a
Hector Nevado <hectornevado46@gmail.com>
2023-11-18 16:09:58 +0100

    Creación estructura API recetas con Laravel 10. Creado rutas, migraciones, factories y modelos

---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\TagController;
+use App\Http\Controllers\Api\RecipeController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+*/
+
+/*
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+*/
+
+Route::get('categories', [CategoryController::class, 'index']);
+Route::get('categories/{category}', [CategoryController::class, 'show']);
+
+Route::get('recipes', [RecipeController::class, 'index']);
+Route::get('recipes/{recipe}', [RecipeController::class, 'show']);
+
+Route::get('tags', [TagController::class, 'index']);
+Route::get('tags/{tag}', [TagController::class, 'show']);
